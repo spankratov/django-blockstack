c6a1f36a3048612c0f68b4a807eeb3fa2d9bd23d
spankratov <ss-pankratov@yandex.ru>
2017-05-06 18:48:26 +0300

    added manifest

--- blockstack_auth/urls.py
+++ blockstack_auth/urls.py
@@ -10,4 +10,5 @@ app_name = 'blockstack'
 urlpatterns = [
     url(r'^blockstack/request', views.blockstack_request, name='request'),
     url(r'^blockstack/response', views.blockstack_response, name='response'),
+    url(r'^blockstack/manifest.json', views.manifest, name='manifest')
 ]
--- blockstack_auth/views.py
+++ blockstack_auth/views.py
@@ -2,16 +2,17 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.urls import reverse
 
 from blockchainauth import AuthRequest
 
 
 def blockstack_request(request):
-    blockstack_auth_settings = settings.BLOCKSTACK_AUTH
+    blockstack_auth_settings = settings.BLOCKSTACK_AUTH['settings']
     blockstack_auth_settings['private_key'] = blockstack_auth_settings.get('private_key', None)
-    blockstack_auth_settings['redirect_uri'] = reverse('blockstack:response')
+    blockstack_auth_settings['manifest_uri'] = blockstack_auth_settings['domain_name'] + reverse('blockstack:manifest')
+    blockstack_auth_settings['redirect_uri'] = blockstack_auth_settings['domain_name'] + reverse('blockstack:response')
     ar = AuthRequest(**blockstack_auth_settings)
     if not AuthRequest.verify(ar.token()):
         return HttpResponse('Can\'t generate Blockstack authentication request token', status=500)
@@ -23,3 +24,9 @@ def blockstack_request(request):
 
 def blockstack_response(request):
     pass
+
+
+def manifest(request):
+    result = JsonResponse(settings.BLOCKSTACK_AUTH['manifest'])
+    result['Access-Control-Allow-Origin'] = '*'
+    return result
